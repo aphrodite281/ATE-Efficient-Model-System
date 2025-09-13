c26fcfe41862d4854b474a3e65c21fc963693410
Aphrodite281 <157574121+aphrodite281@users.noreply.github.com>
2025-09-13 14:24:59 +0800

    Update MainForge.java

--- forge/src/main/java/cn/aph281/ate/ems/forge/MainForge.java
+++ forge/src/main/java/cn/aph281/ate/ems/forge/MainForge.java
@@ -1,8 +1,6 @@
 package cn.aph281.ate.ems.forge;
 
 import cn.aph281.ate.ems.Main;
-import dev.architectury.event.events.client.ClientTextureStitchEvent;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.common.Mod;
 
 @Mod(Main.MOD_ID)
