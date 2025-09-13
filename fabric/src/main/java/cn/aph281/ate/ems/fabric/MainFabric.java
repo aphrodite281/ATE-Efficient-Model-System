8fa5208e5b31941272718c2cebbcbd2102d1e383
aphrodite281 <Aphrodite520@outlook.com>
2025-09-13 08:22:04 +0800

    250913

---
+++ fabric/src/main/java/cn/aph281/ate/ems/fabric/MainFabric.java
@@ -0,0 +1,16 @@
+package cn.aph281.ate.ems.fabric;
+
+import cn.aph281.ate.ems.Main;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.packs.PackType;
+import net.fabricmc.fabric.api.resource.SimpleSynchronousResourceReloadListener;
+import net.fabricmc.fabric.api.resource.ResourceManagerHelper;
+import net.fabricmc.api.ModInitializer;
+import net.minecraft.server.packs.resources.ResourceManager;
+
+public class MainFabric implements ModInitializer {
+    @Override
+	public void onInitialize() {
+
+	}
+}
