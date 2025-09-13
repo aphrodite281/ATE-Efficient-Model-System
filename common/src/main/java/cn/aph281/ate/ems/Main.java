1964e22f23d20c475f676712ec520e4b6cbc60c8
aphrodite281 <Aphrodite520@outlook.com>
2025-09-13 08:22:04 +0800

    250913

---
+++ common/src/main/java/cn/aph281/ate/ems/Main.java
@@ -0,0 +1,14 @@
+package cn.aph281.ate.ems;
+
+import com.google.gson.JsonParser;
+import cn.aph281.ate.ems.shader.ContextCapability;
+import net.minecraft.server.packs.resources.ResourceManager;
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
+
+public class Main {
+    public static final String MOD_ID = "ate-ems";
+
+	public static final Logger LOGGER = LoggerFactory.getLogger("ATE-EMS");
+	public static final JsonParser JSON_PARSER = new JsonParser();
+}
---
+++ forge/src/main/java/cn/aph281/ate/ems/forge/MainForge.java
@@ -0,0 +1,11 @@
+package cn.aph281.ate.ems.forge;
+
+import cn.aph281.ate.ems.Main;
+import dev.architectury.event.events.client.ClientTextureStitchEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(Main.MOD_ID)
+public class MainForge {
+
+}
